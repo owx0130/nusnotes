c01a658c2587c06a58fa9c50691fa9451b5b6f04
unknown <paturi.karthik1@gmail.com>
2024-01-21 05:10:59 +0800

    questions

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import Modules from "../components/modules";
+import Modules from "../components/Modules";
 import WelcomeMessage from "../components/welcome";
 import { useState } from "react";
 import axios from "axios";
-
+import logo from "./image.png";
 export default function Home() {
   const [showModule, setShowModule] = useState(true);
   const [selectedModule, setSelectedModule] = useState({
@@ -18,6 +18,7 @@ export default function Home() {
   });
   const [sumText, setSumText] = useState("");
   const [qnText, setQnText] = useState("");
+  const [showSum, setShowSum] = useState(true);
 
   function handleSelectModule(moduleInfo) {
     setSelectedModule(moduleInfo);
@@ -27,6 +28,7 @@ export default function Home() {
   const handleTopicClick = (module, topic) => {
     axios.get(`${URL}/${module}/${topic}`).then((res) => {
       setSumText(res.data[0].sumText);
+      setQnText(res.data[0].questionText);
     });
   };
 
@@ -38,7 +40,7 @@ export default function Home() {
   return (
     <div className="app">
       <div className="sidebar">
-        <img onClick={()=>reset} src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" />
         <div className="sidebar-button-container">
           <Link to="/">
             <button
@@ -60,61 +62,86 @@ export default function Home() {
       </div>
 
       <div className="main">
-          {showModule ? (
-            <WelcomeMessage />
-          ) : (
+        {showModule ? (
+          <WelcomeMessage />
+        ) : (
+          <div>
+            <div className="top-section-container">
+              {showSum ? (
+                <>
+                  <h2>{selectedModule.module} Summary</h2>
+                  <button
+                    onClick={() => setShowSum(false)}
+                    className="btn btn-dark"
+                  >
+                    Test Yourself!
+                  </button>
+                </>
+              ) : (
+                <>
+                  <h2>{selectedModule.module} Quiz</h2>
+                  <button
+                    onClick={() => setShowSum(true)}
+                    className="btn btn-dark"
+                  >
+                    Review Notes!
+                  </button>
+                </>
+              )}
+            </div>
             <div>
-              <div className="top-section-container">
-                <h2>{selectedModule.module} Summary</h2>
-                <button className="btn btn-dark">Test Yourself!</button>
-              </div>
-              <div>
-                {selectedModule.topics.map((topic, id) => (
-                  <>
-                    <p>
-                      <button
-                        class="btn btn-primary"
-                        type="button"
-                        key={topic}
-                        data-bs-toggle="collapse"
-                        data-bs-target={`#cs1231_${id}`}
-                        aria-expanded="false"
-                        aria-controls={`cs1231_${id}`}
+              {selectedModule.topics.map((topic, id) => (
+                <>
+                  <p>
+                    <button
+                      class="btn btn-primary"
+                      type="button"
+                      key={topic}
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#cs1231_${id}`}
+                      aria-expanded="false"
+                      aria-controls={`cs1231_${id}`}
+                      onClick={() =>
+                        handleTopicClick(selectedModule.module, topic)
+                      }
+                    >
+                      {topic}
+                    </button>
+                  </p>
+
+                  <div class="collapse" id={`cs1231_${id}`}>
+                    <div
+                      class="card card-body"
+                      style={{ backgroundColor: "#ecf0f1" }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-trash"
+                        viewBox="0 0 16 16"
                         onClick={() =>
-                          handleTopicClick(selectedModule.module, topic)
+                          handleDelete(selectedModule.module, topic)
                         }
                       >
-                        {topic}
-                      </button>
-                    </p>
-
-                    <div class="collapse" id={`cs1231_${id}`}>
-                      <div
-                        class="card card-body"
-                        style={{ backgroundColor: "#ecf0f1" }}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-trash"
-                          viewBox="0 0 16 16"
-                          onClick={() =>
-                            handleDelete(selectedModule.module, topic)
-                          }
-                        >
-                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                        </svg>
-                        <pre class="sumtext">{sumText}</pre>
-                      </div>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                      </svg>
+                      <>
+                        {showSum ? (
+                          <pre class="sumtext">{sumText}</pre>
+                        ) : (
+                          <pre class="sumtext">{qnText}</pre>
+                        )}
+                      </>
                     </div>
-                  </>
-                ))}
-              </div>
+                  </div>
+                </>
+              ))}
             </div>
-          )}
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/SubmissionPage.jsx
+++ frontend/src/pages/SubmissionPage.jsx
@@ -1,8 +1,9 @@
 import Submission from "../components/submission";
 import { Link } from "react-router-dom";
-import Modules from "../components/modules";
+import Modules from "../components/Modules";
 import { useState } from "react";
 import axios from "axios";
+import logo from "./image.png";
 
 export default function SubmissionPage() {
   const [showModule, setShowModule] = useState(true);
@@ -19,7 +20,7 @@ export default function SubmissionPage() {
   const [sumText, setSumText] = useState("");
   const [qnText, setQnText] = useState("");
   const [showForm, setShowForm] = useState(true);
-
+  const [showSum, setShowSum] = useState(true);
   function handleSelectModule(moduleInfo) {
     setSelectedModule(moduleInfo);
     setShowModule(false);
@@ -29,6 +30,7 @@ export default function SubmissionPage() {
   const handleTopicClick = (module, topic) => {
     axios.get(`${URL}/${module}/${topic}`).then((res) => {
       setSumText(res.data[0].sumText);
+      setQnText(res.data[0].questionText)
     });
   };
 
@@ -40,7 +42,7 @@ export default function SubmissionPage() {
   return (
     <div className="app">
       <div className="sidebar">
-        <h2 style={{ color: "whitesmoke" }}>NUSnotes</h2>
+      <img src={logo} alt="Logo" />
         <div className="sidebar-button-container">
           <Link to="/">
             <button className="sidebar-button">Home</button>
@@ -75,8 +77,27 @@ export default function SubmissionPage() {
             ) : (
               <>
                 <div className="top-section-container">
+                {showSum ? (
+                <>
                   <h2>{selectedModule.module} Summary</h2>
-                  <button className="btn btn-dark">Test Yourself!</button>
+                  <button
+                    onClick={() => setShowSum(false)}
+                    className="btn btn-dark"
+                  >
+                    Test Yourself!
+                  </button>
+                </>
+              ) : (
+                <>
+                  <h2>{selectedModule.module} Quiz</h2>
+                  <button
+                    onClick={() => setShowSum(true)}
+                    className="btn btn-dark"
+                  >
+                    Review Notes!
+                  </button>
+                </>
+              )}
                 </div>
                 <div>
                   {selectedModule.topics.map((topic, id) => (
@@ -117,7 +138,13 @@ export default function SubmissionPage() {
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                           </svg>
+                          <>
+                        {showSum ? (
                           <pre class="sumtext">{sumText}</pre>
+                        ) : (
+                          <pre class="sumtext">{qnText}</pre>
+                        )}
+                      </>
                         </div>
                       </div>
                     </>
